837f175d1b19a6a7ab3db9439f3f4ab15678a8a7
ungjae <ungjae.yun@gmail.com>
2019-03-31 20:26:15 -0400

    version 2

---
+++ app/src/main/java/com/example/parsegram/fragments/ProfileFragment.java
@@ -0,0 +1,44 @@
+package com.example.parsegram.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.View;
+
+import com.example.parsegram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostsFragment {
+
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> postQuery = new ParseQuery<>(Post.class);
+        postQuery.include(Post.KEY_USER);
+        postQuery.setLimit(20);
+        postQuery.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        postQuery.addDescendingOrder(Post.KEY_CREATED_AT);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                for(int i = 0; i < posts.size(); i++)
+                    Log.d(TAG, "post: " + posts.get(i).getDescription() + "username" + posts.get(i).getUser().getUsername());
+            }
+        });
+    }
+}
